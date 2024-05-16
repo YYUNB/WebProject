b2be5dc5b3a807a7e3623a147c618dc6ed53eaf2
yunnb <chldbsrud010@naver.com>
2024-05-16 23:37:46 +0900

    1

--- javascript/interestBtn.js
+++ javascript/interestBtn.js
@@ -1,3 +1,4 @@
+/*
 let interestBtn = [];
 
 const iContainer = document.getElementById('iContainer');
@@ -8,4 +9,20 @@ for (let i = 0; i < 3; i++) { // 버튼 생성 및 배열에 추가
     button.classList.add('interestBtn');
     iContainer.appendChild(button);
     interestBtn.push(button);
-}
+}*/
+
+var interestContent = [
+    "content1", "content2", "content3"
+];
+/*
+
+var iBtns = document.getElementsByClassName("interestBtn");
+iBtns.forEach((btn, index) => {
+    btn.addEventListener("click", () => {
+        clickBtn(btn)
+    });
+});
+
+function clickBtn(btn) {
+
+}*/
